9f6e19cb43c90418dae80e24a842207cca1103dd
Daniel Hockley <daniel@tforge.co.za>
2023-11-20 15:43:06 +0200

    Updated main.ts to enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+
+    // Enable CORS
+    const corsOptions: CorsOptions = {
+        origin: 'http://localhost:4200', // Update with your frontend URL
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    }
+    app.enableCors(corsOptions)
+
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()

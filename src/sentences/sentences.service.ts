a8ed96c0b35744841c099a047ae49aa647cfedca
Daniel Hockley <daniel@tforge.co.za>
2023-11-23 14:44:21 +0200

    Modify createSentence to handle sentence insertion

--- src/sentences/sentences.service.ts
+++ src/sentences/sentences.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import { Sentence } from './sentence.model'
 
 @Injectable()
 export class SentencesService {
     constructor(
-        @InjectModel('Sentence') private readonly sentenceModel: Model<any>,
+        @InjectModel('Sentence')
+        private readonly sentenceModel: Model<Sentence>,
     ) {}
 
     async getAllSentences() {
